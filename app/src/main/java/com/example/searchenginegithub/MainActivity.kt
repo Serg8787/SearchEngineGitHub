5d4af0c81b032f15f1d1d3bd7b234dc1f4dd4085
Serg8787 <tsybulnikserg@gmail.com>
2021-08-05 09:28:14 +0300

    bt load list

--- app/src/main/java/com/example/searchenginegithub/MainActivity.kt
+++ app/src/main/java/com/example/searchenginegithub/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.searchenginegithub
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -12,29 +12,49 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     var adapter: ProgramistAdapter? = null
+    lateinit var location: String
+    lateinit var language: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        language = ""
+        location = ""
 
-        val loc:String = edLocation.text.toString()
-        val lan:String = edLanguage.text.toString()
+        btSearch.setOnClickListener {
+            getListProgramist()
+        }
+    }
 
-        var retrofit = RetrofitClient.getClient("https://api.github.com/").create(API::class.java)
+    fun getListProgramist() {
+        location = edLocation.text.toString()
+        language = edLanguage.text.toString()
 
-        retrofit.search("location:ukraine language:java")
+        val retrofit = RetrofitClient.getClient("https://api.github.com/").create(API::class.java)
+        // наш запрос https://api.github.com/search/users?q=location:ukraine+language:java
+        // q=означает что у нас в классе API один Query, хотя параметра для вставки два location и language
+        // методом тыка я нашел что вместо + надо просто ставить пробел.
+        // Тут при поиске в Гугле ничего не находил, что поставить вместо +
+
+        retrofit.search("location:${location} language:${language}")
             .enqueue(object : Callback<ResultListProgramist> {
-            override fun onResponse(call: Call<ResultListProgramist>, response: Response<ResultListProgramist>) {
-                if (response.body() != null){
-                    val list:ArrayList<ItemProgramist> = response.body()!!.items as ArrayList<ItemProgramist>
-                    adapter = ProgramistAdapter(baseContext, list)
-                    recycler.setLayoutManager(LinearLayoutManager(baseContext));
-                    recycler.setAdapter(adapter);
+                override fun onResponse(
+                    call: Call<ResultListProgramist>,
+                    response: Response<ResultListProgramist>
+                ) {
+                    if (response.body() != null) {
+                        val list: ArrayList<ItemProgramist> =
+                            response.body()!!.items as ArrayList<ItemProgramist>
+                        adapter = ProgramistAdapter(baseContext, list)
+                        recycler.setLayoutManager(LinearLayoutManager(this@MainActivity));
+                        recycler.setAdapter(adapter);
+                    }
+                    Log.d("MyLOG", "onRespose " + response.body()!!.total_count.toString())
                 }
-                Log.d("MyLOG", "onRespose "+ response.body()!!.total_count.toString())
-            }
-            override fun onFailure(call: Call<ResultListProgramist>, t: Throwable) {
-                Log.d("MyLOG", "onFailture")
-            }
-        })
+
+                override fun onFailure(call: Call<ResultListProgramist>, t: Throwable) {
+                    Log.d("MyLOG", "onFailture")
+                }
+            })
     }
 }
